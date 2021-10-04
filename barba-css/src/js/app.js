39773edfe629d2fb0b715dd8658a056dd3153912
codwerk <louisws@outlook.fr>
2021-10-04 12:31:59 +0200

    Global Hooks
    utiliser les Global Hooks avec Barba.js

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -4,6 +4,14 @@ import barbaCss from '@barba/css';
 // tell Barba to use the css plugin
 barba.use(barbaCss);
 
+const body = document.querySelector('body')
+
+barba.hooks.before( (data) => {
+  const bck = data.current.container.dataset.background
+
+  body.style.setProperty('--page-background', bck)
+})
+
 // init Barba
 barba.init({
   transitions: [
